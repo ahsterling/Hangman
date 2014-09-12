d850a6bdab094b2ada700520f053fd22f3dc6cf4
Allie Sterling <sterlina89@gmail.com>
2014-09-12 16:55:07 -0700

    Back to working order

--- hangman.rb
+++ hangman.rb
@@ -3,23 +3,28 @@ class Hangman
   attr_accessor :words, :current_word, :letters_guessed, :word_guessed, :guess, :counter, :display, :all_words
 
   def initialize
-    @current_word = pick_word_rand.chars
+    @current_word = pick_word_rand
     @letters_guessed = []
-    @word_guessed = word_guessed
+    @word_guessed = empty_characters
     @counter = 0
     @display = Display.new
   end
 
   def all_words
+    #["cat"]
     ["apple", "bacon", "pineapple", "melon", "cheese", "burrito", "avocado"]
   end
 
   def pick_word_rand
-    all_words.sample
+    all_words.sample.chars
   end
 
-  def word_guessed
-    Array.new(@current_word.length, "_ ")
+  def empty_characters
+    Array.new(@current_word.count, "_ ")
+  end
+
+  def display_guess
+    @word_guessed.join(" ")
   end
 
   def array_to_word(array)
@@ -29,30 +34,19 @@ class Hangman
     puts
   end
 
-  def draw_current_word(guess= nil)
-    if guess == nil
-      array_to_word(@word_guessed)
-    else
-      if check(guess)
-        @current_word.each.with_index do |letter, index|
-          if letter == guess
-            @word_guessed[index] = letter
-          end
+  def draw_current_word(guess)
+    if check(guess)
+      @current_word.each.with_index do |letter, index|
+        if letter == guess
+          @word_guessed[index] = letter
         end
-
       end
-
-      array_to_word(@word_guessed)
-
-
-
-    puts
     end
 
   end
 
-  def check(letter)
 
+  def check(letter)
     in_word = @current_word.include? letter
     already_guessed = @letters_guessed.include? letter
     if in_word && !already_guessed
@@ -62,8 +56,6 @@ class Hangman
       @letters_guessed.push(letter)
       @counter += 1
       return false
-    elsif already_guessed
-      return false
     else
       return false
     end
@@ -71,92 +63,25 @@ class Hangman
   end
 
 
-
-  def draw
-
-    grid = {
-      a: "     _______         ",
-      b: "     |/   |          ",
-      c: "     |               ",
-      d: "     |               ",
-      e: "     |               ",
-      f: "     |               ",
-      g: "     |               ",
-      h: "     |_______        ",
-    }
-
-
-    if counter == 0
-
-      grid.each_value {|value| puts value}
-
-    end
-
-    if @counter == 1
-      grid[:c][9] = "(".colorize(:red)
-      grid[:c][10] = "_".colorize(:red)
-      grid[:c][11] = ")".colorize(:red)
-
-      grid.each_value {|value| puts value}
-
-
-    end
-
-    if @counter == 2
-      grid[:d][10] = "|"
-      grid[:e][10] = "|"
-
-
-      grid.each_value {|value| puts value}
-
-    end
-
-#     puts """
-#
-#      _____
-#      |/  |
-#      |  (_)
-#      |   |
-#      | \\|/
-#      |   |
-#      |  / \
-#  ____|______
-# """
-  end
-
   def game_over?
     if @word_guessed == @current_word
+      puts
       puts "Yay, you guessed the word!"
+      puts
       exit
     elsif @counter >= 6
+      puts
       puts "Sorry, you didn't figure out the word in time!  Game over."
+      puts
       exit
     end
   end
 
-
-  def win?
-    if @word_guessed == @current_word
-      puts "Yay, you guessed the word!"
-      exit
-    elsif @counter >= 6
-      puts "Sorry, you didn't figure out the word in time!  Game over."
-      exit
-    end
-
-  end
-
-  def die?
-    if @counter >= 6
-      puts "Sorry, you didn't figure out the word in time!  Game over. "
-    end
-    exit
-  end
 
 end
 
 class Display
-  attr_accessor :grid, :counter
+  attr_accessor :grid
 
   def initialize
     blank_screen
@@ -166,8 +91,9 @@ class Display
     @right_arm = " "
     @left_leg = " "
     @right_leg = " "
-    @counter = 0
   end
+
+
 
   def blank_screen
     @grid = {
@@ -179,41 +105,61 @@ class Display
       f: "     |               ",
       g: "     |               ",
       h: "     |_______        ",
+      j: "                     "
     }
   end
+
+
 
   def draw(counter)
 
     if counter == 0
 
     elsif counter == 1
-      @grid[:c][10] = "0"
+      @grid[:c][10] = "O".colorize(:red)
 
     elsif counter == 2
       @grid[:d][10] = "|"
-      @grid[:e][10] = "|"
+      @grid[:e][10] = "|".colorize(:blue)
 
     elsif counter == 3
-      @grid[:d][9] = "\\"
 
-    elsif counter == 4
       @grid[:d][11] = "/"
 
+    elsif counter == 4
+      @grid[:d][9] = "\\"
+
     elsif counter == 5
-      @grid[:f][9] = "/"
+      @grid[:f][11] = "\\"
 
     elsif counter == 6
-      @grid[:f][11] = "\\"
+
+      @grid[:f][9] = "/"
 
     end
 
-    return @grid.each_value { |value| puts value }
+    @grid.each_value { |value| puts value }
+
   end
 
-  def add_body_part(counter)
 
+end
 
+def correct_input?(input, letters_guessed)
 
+  letters = ("a".."z").to_a
+
+  if input.length != 1
+    puts "Please input one letter at a time."
+    print "> "
+  elsif !letters.include? input.downcase
+    puts "Sorry that isn't a letter.  Please try again."
+    print "> "
+  elsif letters_guessed.include? input
+    puts "You've already guessed that letter.  Please guess again."
+    print "> "
+  else
+    true
   end
 
 end
@@ -221,25 +167,40 @@ end
 def run
 
   h = Hangman.new
-  puts "Hello, welcome to my hangman game!"
+  puts"\e[H\e[2J"
   h.display.draw(0)
-  puts "Here is your word: "
-  h.draw_current_word
+  puts h.display_guess
   prompt = "What letter would you like to guess?"
+  puts
   puts prompt
+  print "> "
   input = gets.chomp
   while input != "exit"
+
+
+    until correct_input?(input, h.letters_guessed)
+      input = gets.chomp
+    end
+
+
+
+    puts"\e[H\e[2J"
+
+
+
     h.draw_current_word(input)
+
     h.display.draw(h.counter)
+    puts h.display_guess
+
     h.game_over?
-    puts prompt
     puts
     puts "You've already guessed these letters: #{h.letters_guessed.join(" ")}"
+    puts
+    puts prompt
+    print "> "
     input = gets.chomp
   end
-
-
-
 
 end
 
